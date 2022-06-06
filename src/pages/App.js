c157215b80d99978de9bc58a3c5095189929b516
Pavel Bezusov <looge.dark@mail.ru>
2022-06-07 00:41:46 +0300

    k12

---
+++ src/pages/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import TitlePage from "./TitlePage";
+
+function App() {
+  return (
+    <div className="App">
+        <Header></Header>
+        <TitlePage></TitlePage>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/TitlePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/main.css'
+
+function TitlePage() {
+    return (
+        <div className="title-page">
+            <div className="photo-pogoda1">
+                Весна-Лето
+            </div>
+            <div className="photo-pogoda2">
+                Осень-Зима
+            </div>
+        </div>
+    );
+}
+
+export default TitlePage;
