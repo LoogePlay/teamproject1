4793e2bf7050cde0b1f95f4ceae7fdf928859baf
Looge <looge.dark@mail.ru>
2022-06-07 16:00:45 +0300

    hjutfgjhg

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,14 +1,22 @@
-import React from "react";
-import Header from "./Header";
-import TitlePage from "./TitlePage";
+import Header from "../components/Header";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import NotFound from "../components/NotFound";
+import Collection from "../components/MainPage";
+import Cart from "../components/cart";
+
 
 function App() {
-  return (
-    <div className="App">
-        <Header></Header>
-        <TitlePage></TitlePage>
-    </div>
-  );
+        return (
+       <div>
+           <Header/>
+            <Router>
+               <Routes>
+                   <Route path='/' element={<Collection/>}/>
+                   <Route path='/cart' element={<Cart/>}/>
+                   <Route path='*' element={<NotFound/>}/>
+               </Routes>
+           </Router>
+       </div>
+    );
 }
-
 export default App;
--- src/pages/TitlePage.js
+++ src/pages/TitlePage.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import '../styles/main.css'
 
-function TitlePage() {
-    return (
-        <div className="title-page">
-            <div className="photo-pogoda1">
-                Весна-Лето
+function TitlePage(){
+        return (
+            <div className="title-page">
+                <div className="photo-pogoda1">
+                    Mans
+                </div>
+                <div className="photo-pogoda2">
+                    Women
+                </div>
             </div>
-            <div className="photo-pogoda2">
-                Осень-Зима
-            </div>
-        </div>
-    );
-}
+        );
+    }
 
 export default TitlePage;
